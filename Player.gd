52796307d27394f767d33af43a1c0fc65b34e3dc
Matthew Oros <orosmatthew@gmail.com>
2019-07-19 22:54:09 -0400

    Adjusted player movement
    fixed bug with holding down opposite direction keys at same time
    fixed bug with sprint animation without moving

--- Player.gd
+++ Player.gd
@@ -2,15 +2,16 @@ extends KinematicBody
 
 var FRICTION = 0.15
 var accel = 5
-var WALK_SPEED = 5
+var WALK_SPEED = 4.25
 var SPRINT_SPEED = WALK_SPEED*1.3
 var SNEAK_SPEED = WALK_SPEED*0.3
-
+var velo = Vector3(0,0,0)
 var velocity = Vector3(0,0,0)
 var velocityGoal = Vector3(0,0,0)
 var velocityGoalPrev = Vector3(0,0,0)
 var mouse_sensitivity = 0.1
 var camera_angle_x = 0
+var sprint_active = false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -18,10 +19,12 @@ func _ready():
 	get_node("HUD/Cross").position = viewportSize/2.0
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("sprint"):
+	if Input.is_action_pressed("sprint") and velo!=Vector3(0,0,0) and sprint_active==false:
+		sprint_active=true
 		$TweenCameraFov.interpolate_property($Camera,"fov", 90, 100, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$TweenCameraFov.start()
-	elif Input.is_action_just_released("sprint"):
+	elif Input.is_action_just_released("sprint") and sprint_active==true:
+		sprint_active=false
 		$TweenCameraFov.interpolate_property($Camera,"fov", 100, 90, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$TweenCameraFov.start()
 	if Input.is_action_pressed("sprint"):
@@ -34,27 +37,33 @@ func _physics_process(delta):
 		velocity.y-=0.5
 	velocity.x-=(velocity.x*FRICTION)
 	velocity.z-=(velocity.z*FRICTION)
-	var velo = Vector3(0,0,0)
+	velo = Vector3(0,0,0)
 	var aim = self.get_rotation_degrees()
 	if Input.is_action_pressed("move_forward"):
 		velo.z+=-cos(deg2rad(aim.y))
 		velo.x+=-sin(deg2rad(aim.y))
-	if Input.is_action_pressed("move_backward"):
+	elif Input.is_action_pressed("move_backward"):
 		velo.z+=cos(deg2rad(aim.y))
 		velo.x+=sin(deg2rad(aim.y))
 	if Input.is_action_pressed("move_left"):
 		velo.x+=-cos(deg2rad(aim.y))
 		velo.z+=sin(deg2rad(aim.y))
-	if Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		velo.x+=cos(deg2rad(aim.y))
 		velo.z+=-sin(deg2rad(aim.y))
 	velocityGoal=velocityGoal.normalized()
 	if velo == Vector3(0,0,0):
 		velocityGoal=Vector3(0,0,0)
+	if velocityGoalPrev.x*velocityGoal.x<0:
+		velocityGoal.x=0
+	if velocityGoalPrev.z*velocityGoal.z<0:
+		velocityGoal.z=0
+		
 	if is_on_floor():
 		velocityGoal+=(velo*0.4)
 	else:
 		velocityGoal+=(velo*0.2)
+		
 	if velocityGoal.x!=0:
 		var angle = atan2(velocityGoal.z,velocityGoal.x)
 		velocity.x = cos(angle)*accel
@@ -70,8 +79,7 @@ func _physics_process(delta):
 
 	velocity = self.move_and_slide(velocity, Vector3(0,1,0))
 	
-func _process(delta):
-	pass
+
 func _input(event):
 	
 	if event is InputEventMouseMotion:
